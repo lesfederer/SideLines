c4ce382c6b055978b749e0e02e2ca760c04c7990
Arthur Federer <114846384+lesfederer@users.noreply.github.com>
2024-08-14 23:27:51 -0400

    Delete src directory

--- src/components/Posts.tsx
+++
@@ -1,9 +0,0 @@
-src/
-  components/
-    Post.jsx
-    // Other components
-  lib/
-    appwrite.js  // Or a similar file for Appwrite interactions
-  pages/
-    home.jsx
-    // Other pages
--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom/cli';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
